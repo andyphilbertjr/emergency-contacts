852da42319dd121efd870fb647b646640238a7af
andyphilbertjr <andyphilbertjr@gmail.com>
2018-09-09 19:15:09 -0400

    optimizations and final build folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox.js';
-import Scroll from './Scroll.js'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll'
 import './App.css'
 
 class App extends React.Component {
@@ -27,10 +27,13 @@ class App extends React.Component {
   }
 
   render (){
-    const filteredContacts = this.state.contacts.filter( person => {
-      return person.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    const { contacts, searchField } = this.state;
+    const filteredContacts = contacts.filter( person => {
+      return person.name.toLowerCase().includes(searchField.toLowerCase())
     })
-    return (
+     return !contacts.length ?
+       <h1>Loading</h1> :
+          (
           <div className='tc'>
             <h1 className='f1'>Emergency Contacts</h1>
             <SearchBox searchChange={this.onSearchChangeHander}/> 
@@ -38,7 +41,7 @@ class App extends React.Component {
               <CardList contacts={filteredContacts} />
             </Scroll>
           </div>
-        )
-  }
+          )
+  } 
 }
 export default App
